e3916aa3c1159c7c2007cdacd89c6e921f3c115c
amanda rossi <alves.rossi@escola.pr.gov.br>
2024-06-20 17:33:28 -0300

    20/06/2024

---
+++ pingpong.js
@@ -0,0 +1,10 @@
+function setup(){
+    //aqui vou criar minha "mesa"
+    createCanvas(600, 400);
+}
+
+    function draw(){
+        //aqui vou por a cor da "mesa"
+        backgroung(255, 255, 255);
+    }
+    
