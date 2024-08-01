2f9e539caef2805992eb11d7a4827f0de592c7c6
amanda rossi <alves.rossi@escola.pr.gov.br>
2024-08-01 14:16:28 -0300

    .bolinha

--- pingpong.js
+++ pingpong.js
@@ -1,22 +1,46 @@
-//aqui vou criar a bolinha
-let xBolinha = 300
-let yBolinha = 200
-let tamBolinha = 25
+//aqui eu vou criar a bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let tamBolinha = 25;
+
+//config velocidade bolinha
+let xvelocidadeBolinha = 6;
+let yvelocidadeBolinha = 6;
 
 function setup(){
     //aqui vou criar minha "mesa"
-    createCanvas(600, 400);
+    createCanvas(600,400);
 }
 
-    function draw(){
-        //aqui vou por a cor da "mesa"
-        //A cor da mesa é em rgb
-        background(178,34,34);
-        //chamando a função
-        criaBolinha (xBolinha, yBolinha, tamBolinha)
-        
+function draw(){
+    //função responsável pelo "desenho e animação da mesa"
+    //aqui vou por a cor da "mesa"
+    //A cor da mesa é em rgb
+    background(128,128,0); 
+    //chamando a função cria bolinha para criar a bolinha
+    criaBolinha(xBolinha, yBolinha, tamBolinha);
+    //chamando a função mov bolinha
+    moveBolinha();
+    //chamando a borda
+    Borda();
+}
+
+//função bolinha
+function criaBolinha(xBolinha, yBolinha,tamBolinha){
+    circle (xBolinha,yBolinha,tamBolinha);
+}
+
+//função move bolinha
+function moveBolinha(){
+    xBolinha = xvelocidadeBolinha + xBolinha;
+    yBolinha = yvelocidadeBolinha + yBolinha;
+}
+
+function Borda(){
+    if (xBolinha > width || xBolinha < 0){
+        xvelocidadeBolinha *= -1;
     }
-    
-    //função bolinha
-    function criaBolinha (xBolinha, yBolinha, tamBolinha);
-        circle (xBolinha, yBolinha, tamBolinha)
+    if (yBolinha > height || yBolinha < 0){
+        yvelocidadeBolinha *= -1;
+    }
+}
